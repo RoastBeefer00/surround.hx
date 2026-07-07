1662757170802897f3222bf88c4fca6e1c5d06fe
Jake Jasmin <roastbeefer000@gmail.com>
2026-07-07 11:37:42 -0600

    fix cog.scm: drop stale helix-vim-plugin dep, no dependencies needed

--- cog.scm
+++ cog.scm
@@ -1,7 +1,6 @@
 (define package-name 'surround.hx)
 (define version "0.1.0")
 
-(define dependencies
-  '((#:name helix-vim-plugin #:git-url "https://github.com/RoastBeefer00/helix-vim-plugin.git")))
+(define dependencies '())
 
 (define dylibs '())
